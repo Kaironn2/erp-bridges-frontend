9bd907224b9aaf29b118a337a12fbf4c664edfb7
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-08-22 17:24:12 -0300

    feat: login and signup pages

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,6 @@
+import { AuthContainer } from '@/components/auth/AuthContainer';
+import { AuthFormSwitcher } from '@/components/auth/AuthFormSwitcher';
+
+export default function SignupPage() {
+  return <AuthFormSwitcher />;
+}
