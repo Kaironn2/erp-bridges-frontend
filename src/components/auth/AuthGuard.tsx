d5a8a6cacd55bd543cf42f2e2add3ceb6dc3bf5f
kaironn2 <jonathas_oliveira17@hotmail.com>
2025-08-23 20:33:24 -0300

    feat: component to prevent authenticated users from accessing authentication routes

---
+++ src/components/auth/AuthGuard.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useAuthStore } from '@/store/use-auth-store';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+type AuthGuardProps = {
+  children: React.ReactNode;
+};
+
+function FullPageLoader() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-background">
+      Something
+    </div>
+  );
+}
+
+export function AuthGuard({ children }: AuthGuardProps) {
+  const { isAuthenticated } = useAuthStore();
+  const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    if (isClient && isAuthenticated) {
+      router.push('/');
+    }
+  }, [isClient, isAuthenticated, router]);
+
+  if (!isClient) {
+    return <FullPageLoader />;
+  }
+
+  if (isClient && isAuthenticated) {
+    return <FullPageLoader />;
+  }
+
+  return <>{children}</>;
+}
