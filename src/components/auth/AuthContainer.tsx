51c81551176dcc6c9256eb6cba9e347d39938ddd
kaironn2 <jonathas_oliveira17@hotmail.com>
2025-08-23 00:34:59 -0300

    chore: AuthContainer now have just one container

--- src/components/auth/AuthContainer.tsx
+++ src/components/auth/AuthContainer.tsx
@@ -1,46 +1,34 @@
 import clsx from 'clsx';
-import Image from 'next/image';
-import Logo from '@/assets/erp-bridges-logo.svg';
+import { ErpBrdigesLogo } from '../branding/ErpBridgesLogo';
+import { ModeToggle } from '../mode-toggle';
 
 type AuthContainerProps = {
   children: React.ReactNode;
 };
 
 export function AuthContainer({ children }: AuthContainerProps) {
-  const leftContainerClasses = clsx(
-    'hidden md:flex',
-    'md:w-3/5 lg:w-7/10',
-    'text-white',
-    'bg-gradient-to-br from-primary to-secondary',
-    'flex flex-col items-center justify-center',
-    'gap-4 px-16'
+  const mainContainerClasses = clsx(
+    'min-h-screen w-full',
+    'flex justify-center',
+    'pt-10',
+    'bg-background'
   );
-  const rightContainerClasses = clsx(
-    'w-full md:w-2/5 lg:w-3/10',
-    'bg-bg-primary',
-    'flex items-center justify-center'
-  );
-  const titleClasses = clsx(
-    'text-center font-extrabold',
-    'md:text-6xl',
-    'lg:text-7xl',
-    'xl:text-8xl'
+
+  const authContainerClasses = clsx(
+    'w-full max-w-md',
+    'flex flex-col items-center',
+    'p-8',
+    'self-start'
   );
 
   return (
-    <div className="flex min-h-screen">
-      <div className={leftContainerClasses}>
-        <Image
-          src={Logo}
-          alt="Erp Bridges logo"
-          priority
-          height={600}
-          width={600}
-        />
-        <h1 className={titleClasses}>ERP Bridges</h1>
+    <div className={mainContainerClasses}>
+      <div className="absolute top-4 right-4">
+        <ModeToggle />
       </div>
 
-      <div className={rightContainerClasses}>
+      <div className={authContainerClasses}>
+        <ErpBrdigesLogo width={150} height={150} withText={true} />
         <div className="p-6 w-full max-w-md">{children}</div>
       </div>
     </div>
