433517888df74545c1c1dacafcd3ecab14989be2
kaironn2 <jonathas_oliveira17@hotmail.com>
2025-08-23 20:31:53 -0300

    chore: auth/ route changed to (auth) route group

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,14 @@
 import { AuthContainer } from '@/components/auth/AuthContainer';
+import { AuthGuard } from '@/components/auth/AuthGuard';
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AuthContainer>{children}</AuthContainer>;
+  return (
+    <AuthGuard>
+      <AuthContainer>{children}</AuthContainer>
+    </AuthGuard>
+  );
 }
