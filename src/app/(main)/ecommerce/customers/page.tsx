33474b3c44ee5a314fceab88d82745c31f609d6b
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-08-27 17:39:40 -0300

    feat: ecommerce customers page

--- src/app/(main)/ecommerce/customers/page.tsx
+++ src/app/(main)/ecommerce/customers/page.tsx
@@ -1,3 +1,23 @@
+import { PageTitle } from '@/components/commom/PageTitle';
+import clsx from 'clsx';
+import { Users } from 'lucide-react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Ecommerce - Clientes',
+};
+
 export default function CustomersPage() {
-  return <h1>Teste!</h1>;
+  return (
+    <div className={clsx('h-full rounded-lg bg-card', 'px-6 pt-8')}>
+      <PageTitle
+        icon={<Users />}
+        title="Ecommerce - Clientes"
+        description="Consulte informações sobre clientes do seu Ecommerce"
+      />
+      <div className="border-y border-muted/50 dark:border-muted-foreground/50 mt-8">
+        <div></div>
+      </div>
+    </div>
+  );
 }
