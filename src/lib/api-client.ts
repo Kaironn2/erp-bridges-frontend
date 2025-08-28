9d04e01dde319e4eb09ad8818a0e451ccbf58589
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-08-28 17:38:31 -0300

    feat: api client

---
+++ src/lib/api-client.ts
@@ -0,0 +1,22 @@
+import { useAuthStore } from '@/store/use-auth-store';
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().accessToken;
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
