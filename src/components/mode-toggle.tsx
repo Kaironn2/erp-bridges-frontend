5b6071856836c9260b089c88419bfa5c96e1b764
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-08-28 17:44:21 -0300

    chore: useEffect to wait mode toggle mount

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,11 +1,20 @@
 'use client';
 
-import * as React from 'react';
 import { useTheme } from 'next-themes';
-import { ThemeSwitch } from './ui/ThemeSwitch';
+import { ThemeSwitch } from './commom/ThemeSwitch';
+import { useEffect, useState } from 'react';
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  });
+
+  if (!mounted) {
+    return null;
+  }
 
   const isDarkMode = theme === 'dark';
 
