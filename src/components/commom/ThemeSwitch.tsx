33f85f0df6561dac5151453c4eb69ad16f83aea1
kaironn2 <jonathas_oliveira17@hotmail.com>
2025-08-29 01:41:58 -0300

    fix: motion (deprecated) changed to motion.create

--- src/components/commom/ThemeSwitch.tsx
+++ src/components/commom/ThemeSwitch.tsx
@@ -7,7 +7,7 @@ import { motion } from 'framer-motion';
 
 import { cn } from '@/lib/utils';
 
-const MotionThumb = motion(SwitchPrimitive.Thumb);
+const MotionThumb = motion.create(SwitchPrimitive.Thumb);
 
 const ThemeSwitch = React.forwardRef<
   React.ComponentRef<typeof SwitchPrimitive.Root>,
