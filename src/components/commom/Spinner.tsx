9f76d1f567d57c5f110d7eb35601c307186ce6db
kaironn2 <jonathas_oliveira17@hotmail.com>
2025-08-30 00:50:29 -0300

    feat: add custom spinner component variants

--- src/components/commom/Spinner.tsx
+++ src/components/commom/Spinner.tsx
@@ -1,23 +1,61 @@
-import clsx from 'clsx';
+import * as React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
 
-export function Spinner() {
-  return (
-    <div className="flex-col gap-4 w-full flex items-center justify-center">
+const spinnerOuterVariants = cva(
+  'text-primary animate-spin flex items-center justify-center rounded-full border-transparent',
+  {
+    variants: {
+      size: {
+        sm: 'w-5 h-5 border-2 border-t-primary',
+        md: 'w-10 h-10 border-4 border-t-primary',
+        lg: 'w-20 h-20 border-4 border-t-primary',
+      },
+    },
+    defaultVariants: {
+      size: 'md',
+    },
+  }
+);
+
+const spinnerInnerVariants = cva(
+  'text-secondary animate-spin flex items-center justify-center rounded-full border-transparent',
+  {
+    variants: {
+      size: {
+        sm: 'w-3 h-3 border-2 border-t-secondary',
+        md: 'w-8 h-8 border-4 border-t-secondary',
+        lg: 'w-16 h-16 border-4 border-t-secondary',
+      },
+    },
+    defaultVariants: {
+      size: 'md',
+    },
+  }
+);
+
+export interface SpinnerProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof spinnerOuterVariants> {}
+
+const Spinner = React.forwardRef<HTMLDivElement, SpinnerProps>(
+  ({ className, size, ...props }, ref) => {
+    return (
       <div
-        className={clsx(
-          'w-20 h-20 border-4 border-transparent border-t-primary',
-          'text-primary text-4xl animate-spin flex items-center',
-          'justify-center rounded-full'
+        className={cn(
+          'flex-col gap-4 w-full flex items-center justify-center',
+          className
         )}
+        ref={ref}
+        {...props}
       >
-        <div
-          className={clsx(
-            'w-16 h-16 border-4 border-transparent border-t-secondary',
-            'text-secondary text-2xl animate-spin flex items-center',
-            'justify-center rounded-full'
-          )}
-        ></div>
+        <div className={cn(spinnerOuterVariants({ size }))}>
+          <div className={cn(spinnerInnerVariants({ size }))}></div>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
+Spinner.displayName = 'Spinner';
+
+export { Spinner };
