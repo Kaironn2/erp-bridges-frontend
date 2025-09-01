24b9df985e9b6d6b2acd11add26f44fe3835f622
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-09-01 16:50:57 -0300

    chore: pointer cursor on accordion trigger

--- src/components/layout/Sidebar/AccordionMenu.tsx
+++ src/components/layout/Sidebar/AccordionMenu.tsx
@@ -53,7 +53,7 @@ export function AccordionMenu({ isCollapsed }: AccordionMenuProps) {
         return (
           <AccordionItem value={`item-${index}`} key={index}>
             <AccordionTrigger
-              className={clsx('p-2 hover:text-primary', {
+              className={clsx('p-2 hover:text-primary cursor-pointer', {
                 'border-l-4 border-primary': isAccordionGroupActive,
               })}
             >
